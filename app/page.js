cff8d16297c0826cdb30fbe669aecc96f362dc72
arya205 <syarifrahmanbego@gmail.com>
2025-06-01 23:33:04 +0700

    test2

--- app/page.js
+++ app/page.js
@@ -29,8 +29,8 @@ export default async function Home() {
     <div suppressHydrationWarning >
       <HeroSection />
       <AboutSection />
-      {/* <Experience />
-      <Skills />
+      <Experience />
+      {/* <Skills />
       <Projects />
       <Education />
       <Blog blogs={blogs} />
