8515257cd85f426065c3def68cba99c781be1509
arya205 <syarifrahmanbego@gmail.com>
2025-06-01 22:59:19 +0700

    test

---
+++ app/components/toastContainer/toastContainer.jsx
@@ -0,0 +1,12 @@
+'use client'
+import { ToastContainer } from 'react-toastify'
+
+function ToastContainerComp() {
+  return (
+    <>
+      <ToastContainer />
+    </>
+  )
+}
+
+export default ToastContainerComp
